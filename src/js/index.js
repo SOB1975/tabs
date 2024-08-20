12f356813a8bfc966b6df0aded99d1f6d92d9d2e
Oleg <stroleg912@gmail.com>
2024-08-20 11:12:05 +0300

    add js

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,24 @@ Fancybox.bind('[data-fancybox]', {
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+// небольшой скрипт, который будет отвечать за переключение контента при нажатии на вкладки:
+
+document.addEventListener('DOMContentLoaded', function() {
+  var tabItems = document.querySelectorAll('.tab-item');
+ 
+  tabItems.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+      var tabId = this.getAttribute('data-tab');
+      var content = document.getElementById(tabId);
+ 
+      // Удаляем активный класс у текущих вкладок и контента
+      document.querySelector('.tab-item.active').classList.remove('active');
+      document.querySelector('.tab-pane.active').classList.remove('active');
+ 
+      // Добавляем активный класс для выбранной вкладки и контента
+      this.classList.add('active');
+      content.classList.add('active');
+    });
+  });
+});
